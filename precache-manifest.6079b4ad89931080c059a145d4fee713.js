a3acef644e61837040754fab966878084a05cf31
gandersen <george.andersen@obextech.com>
2020-08-18 09:32:25 +0100

    Updates

---
+++ precache-manifest.6079b4ad89931080c059a145d4fee713.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcccea8be04b1cdf80e862f54ed1fc32",
+    "url": "/bible-tools/index.html"
+  },
+  {
+    "revision": "27cfb80d59fc596233a4",
+    "url": "/bible-tools/static/css/main.5a235be7.chunk.css"
+  },
+  {
+    "revision": "fe991ff657dc1bd4675f",
+    "url": "/bible-tools/static/js/2.06ccceb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bible-tools/static/js/2.06ccceb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27cfb80d59fc596233a4",
+    "url": "/bible-tools/static/js/main.db90dba7.chunk.js"
+  },
+  {
+    "revision": "34994231113d99b36581",
+    "url": "/bible-tools/static/js/runtime-main.b947f49e.js"
+  }
+]);
