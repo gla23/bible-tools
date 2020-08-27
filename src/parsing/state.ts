b077c25f37b937509ecf3ca2926f97201a81e615
gandersen <george.andersen@obextech.com>
2020-08-27 08:31:28 +0100

    Use class properly

--- src/parsing/state.ts
+++ src/parsing/state.ts
@@ -15,51 +15,6 @@ type ValueState = typeof valueStates[number];
 const isValueState = (state: StateType): state is ValueState =>
   valueStates.indexOf(state as ValueState) !== -1;
 
-function transition(this: State, input: Input): State {
-  const newStateType = nextStateType(this.type, input.type);
-  const newState: State = new State(this);
-  newState.type = newStateType;
-  newState.testament = input.testament || this.testament;
-  newState.inputs = [...this.inputs, input];
-  newState.string = this.string + input.string;
-  newState.error = input.error || this.error;
-  if (input.type === "number" && isValueState(newStateType)) {
-    newState[newStateType] = input;
-  }
-  return newState;
-}
-
-function toReference(state: State): string {
-  const {
-    testament,
-    book,
-    chapter,
-    verse,
-    bookEnd,
-    chapterEnd,
-    verseEnd,
-  } = state;
-  const books = testament === "o" ? otBooks : ntBooks;
-  const sectionTo =
-    (bookEnd ? books[(bookEnd.value || 1) - 1] + " " : "") +
-    (chapterEnd?.value || "") +
-    (chapterEnd && verseEnd ? ":" : "") +
-    (verseEnd?.value || "");
-  return (
-    (book ? books[(book.value || 1) - 1] : "") +
-    (chapter ? " " + chapter.value : "") +
-    (verse ? ":" + verse.value : "") +
-    (sectionTo ? "-" + sectionTo : "")
-  );
-}
-
-function inputType(this: State, input: Input): string {
-  for (let valueState of valueStates) {
-    if (this[valueState] === input) return valueState;
-  }
-  return input.type;
-}
-
 export type Testament = "o" | "n";
 
 export class State {
@@ -78,11 +33,6 @@ export class State {
       this.error = state.error;
     }
   }
-  transition: (this: State, input: Input) => State = transition;
-  inputType: (this: State, input: Input) => string = inputType;
-  get reference(): string {
-    return toReference(this);
-  }
   type: StateType = "initial";
   testament: Testament | null = null;
   book: Input | null = null;
@@ -94,6 +44,49 @@ export class State {
   inputs: Input[] = [];
   string: string = "";
   error: ParseError | null = null;
+
+  transition(this: State, input: Input): State {
+    const newStateType = nextStateType(this.type, input.type);
+    const newState: State = new State(this);
+    newState.type = newStateType;
+    newState.testament = input.testament || this.testament;
+    newState.inputs = [...this.inputs, input];
+    newState.string = this.string + input.string;
+    newState.error = input.error || this.error;
+    if (input.type === "number" && isValueState(newStateType)) {
+      newState[newStateType] = input;
+    }
+    return newState;
+  }
+  inputType(this: State, input: Input): string {
+    for (let valueState of valueStates) {
+      if (this[valueState] === input) return valueState;
+    }
+    return input.type;
+  }
+  get reference(): string {
+    const {
+      testament,
+      book,
+      chapter,
+      verse,
+      bookEnd,
+      chapterEnd,
+      verseEnd,
+    } = this;
+    const books = testament === "o" ? otBooks : ntBooks;
+    const sectionTo =
+      (bookEnd ? books[(bookEnd.value || 1) - 1] + " " : "") +
+      (chapterEnd?.value || "") +
+      (chapterEnd && verseEnd ? ":" : "") +
+      (verseEnd?.value || "");
+    return (
+      (book ? books[(book.value || 1) - 1] : "") +
+      (chapter ? " " + chapter.value : "") +
+      (verse ? ":" + verse.value : "") +
+      (sectionTo ? "-" + sectionTo : "")
+    );
+  }
 }
 
 export const initialState = new State();
